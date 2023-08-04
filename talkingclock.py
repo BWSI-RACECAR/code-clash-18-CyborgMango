75c7c07da9a9de63b2379017a33974d7e3882cee
CyborgMango <129334600+CyborgMango@users.noreply.github.com>
2023-08-04 09:16:00 -0400

    Add files via upload

--- talkingclock.py
+++ talkingclock.py
@@ -42,7 +42,7 @@ class Solution:
         hourL=["one ","two ","three ","four ","five ","six ","seven ","eight ","nine ","ten ","eleven ","twelve "]
         min1L=["oh","one ","two ","three ","four ","five "]
         min2L=["","one ","two ","three ","four ","five ","six ","seven ","eight ","nine "]
-        min2teenL=[" ten"," eleven"," twelve"," thirteen"," fourteen"," fifteen"," sixteen"," seventeen"," eighteen"," nineteen"]
+        min2teenL=["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
         if min1=='0':
             if min2=='0':
                 mins=''

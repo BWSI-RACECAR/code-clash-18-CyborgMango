cdae3f8b079441652bbe1d8a3e6eb18d6bad447e
CyborgMango <129334600+CyborgMango@users.noreply.github.com>
2023-08-04 09:12:40 -0400

    Add files via upload

---
+++ talkingclock.py
@@ -0,0 +1,67 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2023
+
+Code Clash #18 - Talking Clock (talkingclock.py)
+
+
+Author: Andrew Scott White
+
+Difficulty Level: 8/10
+
+Prompt: I don’t want to be late for the BWSI Grand Prix, so I want
+to program my phone to update me on the time. Can you help me make 
+a Talking Clock? I need a script that takes an input 24-hour time 
+(00:00 - 23:59) and translates it into words. Please format the input 
+as ‘##:##’ and include am/pm in the output string. Thanks!
+
+Test Cases:
+Input: 12:00  Output: It's twelve pm
+
+Input: 23:59  Output: It's eleven fifty nine pm
+
+Input: 12:05  Output: It's twelve oh five pm
+"""
+
+
+
+
+    # This will convert military hours to regular hours, and determine AM vs PM
+class Solution:    
+    def ClockTalker(self, input_time):
+        hour= input_time[0:1]
+        min1=input_time[3]
+        min2=input_time[4]
+        if int(hour)>=12:
+            ampm=" pm"
+            hour=int(hour)-12
+        else:
+            ampm=" am"
+        hourL=["one ","two ","three ","four ","five ","six ","seven ","eight ","nine ","ten ","eleven ","twelve "]
+        min1L=["oh","one ","two ","three ","four ","five "]
+        min2L=["","one ","two ","three ","four ","five ","six ","seven ","eight ","nine "]
+        min2teenL=[" ten"," eleven"," twelve"," thirteen"," fourteen"," fifteen"," sixteen"," seventeen"," eighteen"," nineteen"]
+        if min1=='0':
+            if min2=='0':
+                mins=''
+            mins="oh "+min2L[min2]
+        elif min1=='1':
+            mins=min2teenL[min2]
+        else:
+            mins=min1L[min1]+min2L[min2]
+        print("It's "+str(hour)+str(mins)+ampm)
+            #type input_time: string
+            #return type: string
+            
+            #TODO: Write code below to return a string with the solution to the prompt.
+
+def main():
+     str1=input()
+     tc1= Solution()
+     ans=tc1.ClockTalker(str1)
+     print(ans)
+    
+if __name__ == '__main__': 
+    main()
+        
